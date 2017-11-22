84748b1cef0eb832334b3da9b00e728a00243252
Исабеков Ильяс Серикович <@corp.itstep>
2017-11-22 11:46:40 +0600

    Practic 1,2

---
+++ Practic 1,2/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Введите первое число");
+                    int x = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число");
+                    int y = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Сумма : {2}", x, y, x + y);
+
+                }
+                while (Console.ReadKey().Key != ConsoleKey.Escape);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
+
